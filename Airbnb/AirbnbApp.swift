bf89eb3cea218d4e688f35d5810b129ad71e477a
Xojimurod <151165550+iOSDew@users.noreply.github.com>
2025-03-09 06:17:14 +0500

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Khojimurod Sultonov on 09/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
